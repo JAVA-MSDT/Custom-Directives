595740e220cceb242914a9e1298e1baec13ba4f5
JAVA-MSDT <serenitydiver@hotmail.com>
2022-02-02 15:21:40 +0100

    In progress for trigger press directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StyleParentDirective } from './directive/style-parent.directive';
+import { TriggerPressDirective } from './directive/trigger-press.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, StyleParentDirective ],
+  declarations: [ AppComponent, HelloComponent, StyleParentDirective, TriggerPressDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/directive/trigger-press.directive.ts
+++ src/app/directive/trigger-press.directive.ts
@@ -1,24 +1,29 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { KEY_BUTTON } from '../util/buttons';
 
 @Directive({
-  selector: '[appTriggerPress]'
+  selector: '[appTriggerPress]',
 })
 export class TriggerPressDirective {
-
-  constructor(private el: ElementRef) { }
+  eventFired: KeyboardEvent;
+  constructor(private el: ElementRef) {
+    this.eventFired = this.el.nativeElement.event;
+    console.log(el.nativeElement)
+  }
 
   @HostListener('keydown') triggerPress() {
-
+    console.log(this.eventFired);
   }
 
   buttonPressHandler(event: KeyboardEvent, element: HTMLElement): void {
-		const isClick =
-			['Enter', 'Space'].includes(event.code) || [KEY_BUTTON.RETURN, KEY_BUTTON.SPACE].includes(event.keyCode);
+    const isClick =
+      ['Enter', 'Space'].includes(event.code) ||
+      [KEY_BUTTON.RETURN, KEY_BUTTON.SPACE].includes(event.keyCode);
 
-		if (isClick) {
-			element.click();
-			event.preventDefault();
-			event.stopPropagation();
-		}
-	}
+    if (isClick) {
+      element.click();
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
 }
