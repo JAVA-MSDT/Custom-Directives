22bdcafc21cddd731fc743cefdf0f42cedd9dc1d
JAVA-MSDT <serenitydiver@hotmail.com>
2022-02-02 16:20:42 +0100

    enhancment for the demonstration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,19 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
   displayText = false;
+  displaySpan = false;
+
   toggleDisplay(): void {
-this.displayText = !this.displayText;
+    this.displayText = !this.displayText;
+  }
+
+  toggleDisplaySpan(): void {
+    this.displaySpan = !this.displaySpan;
   }
 }
